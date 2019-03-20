777ac7a809b7e9a14630de5f92a3dcb0162e8ba0
reactionaryzebra <theSource@Justins-MacBook-Air.local>
2019-03-20 13:39:11 -0700

    Commit 3 - Excited Kittens answered

--- 3-20/js/app.js
+++ 3-20/js/app.js
@@ -7,3 +7,12 @@ for (let i = 1; i < 21; i++) {
 for (let i = 0; i < 201; i+=2) {
   console.log(i);
 }
+
+//Question 3
+const messages = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."];
+for (let i = 0; i < 20; i++) {
+  console.log("Love me, pet me! HSSSSSS!");
+  if (i % 2 === 0){
+    console.log(messages[Math.floor(Math.random() * 3)]);
+  }
+}
